a0e15ddca60f7e4b25604e7a85313ff6ad39bc6c
kush <kusumsikhwal44@gmail.com>
2022-02-09 12:03:46 +0530

    add new req

--- bank_pro/settings.py
+++ bank_pro/settings.py
@@ -109,14 +109,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
